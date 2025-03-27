07b11c2572eb913d79c0f35d1d3c6de93f128667
hsienlc <1202ella2219@gmail.com>
2025-03-27 14:00:49 +0800

    Update test_fizzbuzz.py

--- UnitTestHW/Fizz Buzz/Class/test_fizzbuzz.py
+++ UnitTestHW/Fizz Buzz/Class/test_fizzbuzz.py
@@ -8,16 +8,12 @@ def solution():
 class TestSolution:
     def test_3(self, solution):
         assert solution.singleFizzBuzz(3) == "Fizz"
-    
     def test_5(self, solution):
         assert solution.singleFizzBuzz(5) == "Buzz"
-    
     def test_15(self, solution):
         assert solution.singleFizzBuzz(15) == "FizzBuzz"
-    
     def test_2(self, solution):
         assert solution.singleFizzBuzz(2) == "2"
-
     def test_main_14(self, solution):
         assert solution.fizzBuzz(14) == ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14"]
     def test_main_15(self, solution):
